a1813e8608427bb05fc79e619249632d0430c628
creewick <creewick@ya.ru>
2018-02-13 09:52:26 +0500

    global counter, new tasks

--- onload.js
+++ onload.js
@@ -2,14 +2,18 @@ var data;
 $.get('./data.json', (context) => {
     data = context;
     let checkboxes = $('[name=tag]');
+    let total = 0;
     for (let i = 0; i < checkboxes.length; i++){
         let checkbox = checkboxes[i];
         let count = 0;
         let sheets = filter(data);
         let tasks = getTasks(sheets);
         for (let j = 0; j < tasks.length; j++)
-            if (tasks[j].From.Tag === checkbox.value)
+            if (tasks[j].From.Tag === checkbox.value) {
                 count++;
+                total++;
+            }
         checkbox.parentElement.innerHTML += `(${count})`;
     }
+    $('h1')[0].innerText += ` (${total})`;
 });
